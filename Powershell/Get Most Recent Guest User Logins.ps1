a9c1281ba16f70be979f71f48c5f1b337451e004
jontiadcock <jontiadcock@gmail.com>
2022-08-17 10:48:34 +0800

    Big update, working 100%

--- Powershell/Get Most Recent Guest User Logins.ps1
+++ Powershell/Get Most Recent Guest User Logins.ps1
@@ -1,69 +1,86 @@
 ﻿# This script Takes a list of all Azure AD Guest Users and records most recent login (Interactive, or non interactive)
 # Created by Jonti :)
 
-########## VARIABLES - CHANGE ME ##########
-
-$tenantAddr = "@mytenant.onmicrosoft.com" # This is the tenant default @***.onmicrosoft.com domain
-$tenantdomain = "@mytetant.com.au" # This is the tenant primary domain
-$mstimeperuser = "500" # Increase number if getting error: Error occurred while executing GetAuditSignInLogs, 500 is normally pretty good but may need to be upped to 1000.
-# If you have many users to audit, then 500 normally will give best results in quickest time. If you only have <50 users, then setting to 1000 will be fine.
 
 # $Type = "guests" #or "regularusers" # this variable isnt working yet
 
-########## VARIABLES - CHANGE ME ##########
-
 
 # Start up warning
-Write-Output "Run PowerShell as Administrator!
-If you havent run as admin, press 'Ctrl + c' a few times to cancel, and re-launch as admin
-Please click 'Yes to all' to continue, otherwise script will not work"
-Start-Sleep -Milliseconds 15000
+Write-Output "Run PowerShell as Administrator!"
+$Admin = read-host -p "'Did you run as admin? (Y/N)'"
+if ($Admin -eq "Y") {
+    Write-Output "Starting script..."
 
-# Uninstall Azure Module and install AzureAD Preview Module
-Write-Output "Uninstalling standard modules and changing to preview modules"
-Uninstall-Module -Name AzureAD
-Install-Module -Name AzureADPreview
+    # ask for script parameters details
+    $TenantAddr = read-host -p 'Enter the Tenat full address of the Azure tenancy, example: @mytenant.onmicrosoft.com (include @)'
+    $tenantdomain = read-host -p 'Enter the primary domain of the tenant, example: @mydomain.com.au (include @)'
+    $pathforcsv = read-host -p "Enter the path to the csv file you want to save the results to, example: 'C:\Users\john.doe\results.csv'"
+    $mstimeperuser = "1000" # Increase number if getting error: Error occurred while executing GetAuditSignInLogs, 650 is normally pretty good but may need to be upped to 1000 if getting errors.
+    # If you have many users to audit, then 1000 normally will give best results in quickest time. If you only have <50 users, then setting to 1000 will be fine.
 
-# Connect to AZAD
-Write-Output "Connecting to AzureAD"
-Connect-AzureAD
-Start-Sleep -Milliseconds 10000
+    # Uninstall Azure Module and install AzureAD Preview Module
+    Write-Output "Please click 'Yes to all' to continue, otherwise script will not work"
+    Write-Output "Uninstalling standard modules and changing to preview modules"
+    Uninstall-Module -Name AzureAD | out-null
+    Install-Module -Name AzureADPreview | out-null
 
-# Generate List of guest users by email address and assign to $userlist variable
-Write-Output "This process takes around 1-3 seconds per user and can take time to complete, there is no status updates aside from errors sometimes, please be patient and do not open CSV before completion"
-$userlist = Get-AzureADUser -All $true | Select-Object -Unique "UserPrincipalName" | Select-String -pattern "#EXT#"
-$userlist = $userlist -replace "@{UserPrincipalName=" -replace "}" -replace "#EXT#" -replace "$TenantAddr" -replace "$tenantdomain" -replace "_", "@"
+    # Connect to AZAD
+    Write-Output "Connecting to AzureAD"
+    Connect-AzureAD | Out-Null
+    Start-Sleep -Milliseconds 5000
 
-# Check for login data for each user and export to CSV
-ForEach ($singleuser in $userlist) {
-$tempvalue = Get-AzureADAuditSignInLogs -Filter "UserPrincipalName eq '$singleuser'" -Top 1 | Select-Object CreatedDateTime, UserPrincipalName, userDisplayName
-Start-Sleep -Milliseconds $mstimeperuser
-    if ($tempvalue -eq $null){
-      Write-Output "$singleuser`tNo login for over 1month" | Out-File C:\temp\AzureGuestUsers.csv -Append
-      Start-Sleep -Milliseconds $mstimeperuser
-      }
+    # Generate List of guest users by email address and assign to $userlist variable
+    $userlist = Get-AzureADUser -All $true | Select-Object -Unique "UserPrincipalName" | Select-String -pattern "#EXT#"
+    $userlist = $userlist -replace "@{UserPrincipalName=" -replace "}" -replace "#EXT#" -replace "$TenantAddr" -replace "$tenantdomain" -replace "_", "@"
+
+    # Create CSV file and write headers
+    Write-Output "Creating CSV file"
+    Write-Output "Ignore" | Out-File -FilePath $pathforcsv
+
+    # Check for login data for each user and export to CSV
+    Write-Output "Getting and Writing Data, this process takes around 1-3 seconds per user and can take time to complete, there is no status updates aside from errors sometimes, please be patient and do not open CSV before completion"
+    ForEach ($singleuser in $userlist) {
+    $tempuservalue = Get-AzureADAuditSignInLogs -Filter "UserPrincipalName eq '$singleuser'" -Top 1 | Select-Object CreatedDateTime, UserPrincipalName, userDisplayName
+    Start-Sleep -Milliseconds $mstimeperuser
+        if ($null -eq $tempuservalue){
+          Write-Output "$singleuser`tNo login for over 1month" | Out-File -FilePath "$pathforcsv" -append
+          Start-Sleep -Milliseconds $mstimeperuser
+          }
+        else {
+          Write-Output "$tempuservalue" |
+          ForEach-Object {$_ -Replace '@{CreatedDateTime=', 'Most recent login: '} |
+          ForEach-Object {$_ -Replace '; ',', '} |
+          ForEach-Object {$_ -Replace '}',' '} |
+          ForEach-Object {$_ -Replace 'UserPrincipalName=', 'Email: '} |
+          ForEach-Object {$_ -Replace ' UserDisplayName=', ' Name: '} |
+          Out-File -FilePath "$pathforcsv" -append
+          Start-Sleep -Milliseconds $mstimeperuser
+        }
+    Start-Sleep -Milliseconds $mstimeperuser
+    }
+
+    # Disconnect AzureAD
+    Write-Output "Disconnecting AzureAD"
+    Disconnect-AzureAD
+
+    # Shut down azuread module
+    Write-Output "Shutting down AzureAD Preview module"
+    Remove-Module -Name AzureADPreview
+    Remove-Module -Name PowerShellGet | out-null
+
+    # Reinstall standard AzureAD module
+    Write-Output "Reinstalling standard modules"
+    Uninstall-Module -Name AzureADPreview | out-null
+    Install-Module -Name AzureAD | out-null
+
+    # End script
+    Start-Sleep -Milliseconds 5000
+    Write-Output "Script Complete, file located in '$pathforcsv'"
+    Start-Sleep -Milliseconds 5000
+    exit
+  }
+    # End script if user did not run as admin
     else {
-      Write-Output "$tempvalue" |
-      ForEach-Object {$_ -Replace '@{CreatedDateTime=', 'Most recent login: '} |
-      ForEach-Object {$_ -Replace '; ',', '} |
-      ForEach-Object {$_ -Replace '}',' '} |
-      ForEach-Object {$_ -Replace 'UserPrincipalName=', 'Email: '} |
-      ForEach-Object {$_ -Replace ' UserDisplayName=', ' Name: '} |
-      Out-File C:\temp\AzureGuestUsers.csv -Append
-      Start-Sleep -Milliseconds $mstimeperuser
-     }
-Start-Sleep -Milliseconds $mstimeperuser
-}
-
-# Disconnect AzureAD
-Write-Output "Disconnecting AzureAD"
-Disconnect-AzureAD
-
-# Reinstall standard AzureAD module
-Write-Output "Reinstalling standard modules"
-Uninstall-Module -Name AzureADPreview
-Install-Module -Name AzureAD
-
-# End script
-Start-Sleep -Milliseconds 5000
-Write-Output "Script Complete, file located in C:\temp\AzureGuestUsers"
+    Write-Output "Exiting script... Run as admin next time"
+    exit
+    }
